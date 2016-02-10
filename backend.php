d58fc0206886013c32c4b6d7aa4fa9dae53cf7d1
azhary <engineeringwithlove@gmail.com>
2016-02-10 14:57:20 +0700

    testing uwp

---
+++ backend.php
@@ -0,0 +1,23 @@
+<?php 
+define("HOSTNAME", "br-cdbr-azure-south-a.cloudapp.net");
+define("USERNAME", "b89ed51f5d9bbe");
+define("PASSWORD", "3f2785a2");
+define("DBNAME", "db_azurems");
+
+@mysql_connect(HOSTNAME, USERNAME, PASSWORD) or die(mysql_error());
+@mysql_select_db(DBNAME);
+
+$nama = $_POST["nama"];
+$email = $_POST["email"];
+if (isset($nama, $email)) {
+	@mysql_query("INSERT INTO tamu VALUES('".$nama."', '".$email."')");
+}
+
+$sql = @mysql_query("SELECT * FROM tamu");
+if (mysql_num_rows($sql) > 0) {
+	$result = mysql_result($sql);
+	while ($row = mysql_fetch_array($result)) {
+		echo $row;
+	}
+}
+?>
---
+++ hello.php
@@ -0,0 +1,5 @@
+<?php 
+session_start();
+$_SESSION['nama'] = "Yoga";
+?>
+<a href="world.php">Berikut</a>
